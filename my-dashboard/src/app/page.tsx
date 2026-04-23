7d1c05aac668fb247ac70bcb4705bfe8c562dff7
refreshedmayank <refreshedmayank@gmail.com>
2026-04-23 14:12:28 +0530

    styled cards

--- my-dashboard/src/app/page.tsx
+++ my-dashboard/src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 function Home(){
   return(<div className="outer wall flex h-screen">
 
-<div className="border flex flex-col items-center justify-center">
+<div className="bg-black text-white border flex flex-col items-center justify-center p-6">
   <span>icon-01</span>
   <span>icon-02</span>
   <span>icon-03</span>
@@ -10,8 +10,8 @@ function Home(){
   <span>icon-05</span>
   <span>icon-06</span>
 </div>
-<div className="border dashboard flex-1">
-  <div className="Header flex justify-between items-center border ">
+<div className=" dashboard flex-1 ">
+  <div className="Header bg-black text-white flex justify-between items-center border mb-4 p-6">
     <span>Expenser</span>
     <div className="navitems flex gap-6 items-center">
       <Link href="">home</Link>
@@ -20,20 +20,22 @@ function Home(){
       <input className="border" type="text" placeholder="search" />
     </div>
   </div>
-  <div className="Card">
+  <div className="Cards-grid grid grid-cols-3 gap-3">
+  <div className="Card border p-6 rounded flex flex-col justify-center items-center gap-2 h-48 rounded-xl">
     <span>Monthly Income</span>
     <span>$40,000</span>
   </div>
-  <div className="Card-2">
+  <div className="Card-2 border p-6 rounded flex flex-col gap-2 justify-center items-center rounded-xl ">
     <span>Monthly Expenses</span>
     <span>67%</span>
   </div>
-  <div className="Card-3">
+  <div className="Card-3 border p-6 rounded flex flex-col gap-2 justify-center items-center rounded-xl">
     <span>Savings</span>
     <span>$13,200</span>
   </div>
-  <div className="border">Chart</div>
-  <div className="border">Table</div>
+  </div>
+  <div className="border p-9">Chart</div>
+  <div className="border p-9  ">Table</div>
 </div>
 
 
