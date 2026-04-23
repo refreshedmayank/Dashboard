22811ea4eb9e722221f5eb98b265bd2f7188ae6d
refreshedmayank <refreshedmayank@gmail.com>
2026-04-23 14:20:57 +0530

    deleted table and turned dashboard theme to dark

--- my-dashboard/src/app/page.tsx
+++ my-dashboard/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 function Home(){
-  return(<div className="outer wall flex h-screen">
+  return(<div  className="outer wall flex h-screen">
 
 <div className="bg-black text-white border flex flex-col items-center justify-center p-6">
   <span>icon-01</span>
@@ -10,7 +10,7 @@ function Home(){
   <span>icon-05</span>
   <span>icon-06</span>
 </div>
-<div className=" dashboard flex-1 ">
+<div className="bg-black text-white dashboard flex-1 ">
   <div className="Header bg-black text-white flex justify-between items-center border mb-4 p-6">
     <span>Expenser</span>
     <div className="navitems flex gap-6 items-center">
@@ -21,21 +21,20 @@ function Home(){
     </div>
   </div>
   <div className="Cards-grid grid grid-cols-3 gap-3 p-4">
-  <div className="Card border p-6 rounded flex flex-col justify-center items-center gap-2 h-48 rounded-xl">
+  <div className="Card border p-6 rounded flex flex-col justify-center items-center gap-2 h-48 rounded-xl bg-black text-white">
     <span>Monthly Income</span>
     <span>$40,000</span>
   </div>
-  <div className="Card-2 border p-6 rounded flex flex-col gap-2 justify-center items-center rounded-xl ">
+  <div className="Card-2 border p-6 rounded flex flex-col gap-2 justify-center items-center rounded-xl bg-black text-white">
     <span>Monthly Expenses</span>
     <span>67%</span>
   </div>
-  <div className="Card-3 border p-6 rounded flex flex-col gap-2 justify-center items-center rounded-xl">
+  <div className="Card-3 border p-6 rounded flex flex-col gap-2 justify-center items-center rounded-xl bg-black text-white">
     <span>Savings</span>
     <span>$13,200</span>
   </div>
   </div>
-  <div className="border p-9">Chart</div>
-  <div className="border p-9  ">Table</div>
+  <div className="border p-9">Daily Expenses</div>
 </div>
 
 
